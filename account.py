e4da343d7141ddd502d31fe922895a819008507d
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-06-17 09:49:21 +1000

    done account started bank

--- account.py
+++ account.py
@@ -4,4 +4,50 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+class Account:
+    def __init__(self, name, acc_num, balance):
+        self.set_name(name)
+        self.set_acc_num(acc_num)
+        self.set_balance(balance)
 
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if name != "":
+            self._name = name
+        else:
+            raise ValueError("not a valid name")
+
+    def get_acc_num(self):
+        return self._acc_num
+    
+    def set_acc_num(self, acc_num):
+        if isinstance(acc_num, int) and acc_num >= 0:
+                self._acc_num = acc_num
+        else:
+            raise ValueError("Not a valid int")
+
+    def get_balance(self):
+        return self._balance
+    
+    def set_balance(self, balance):
+        if isinstance(balance, float, int) and balance >= 0:
+                self._acc_num = balance
+        else:
+            raise ValueError("Not a valid int")
+        
+    def deposit(self, balance, deposit_amount):
+        if deposit_amount > 0:
+            self._balance = balance + deposit_amount
+        else:
+            raise ValueError("Not a valid deposit")
+        
+    def withdraw(self, balance, withdraw_ammount):
+        if withdraw_ammount > 0 and balance - withdraw_ammount >= 0:
+            self._balance = balance - withdraw_ammount
+        else:
+            raise ValueError("Not a valid withdraw")
+         
+        
+    
--- bank.py
+++ bank.py
@@ -4,3 +4,17 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+from account import Account
+
+class Bank:
+    def __init__(self):
+        self._accounts = []
+
+    def add_account
+
+    def account_exist(self, acc_num):
+        for account in self._accounts:
+            if account.get_acc_num() == acc_num:
+                return True
+            else:
+                return False
