2cd0c64ee9c320d587fd8fce07900e91b93f6eca
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-06-18 11:49:04 +1000

    bank  almost domne

--- account.py
+++ account.py
@@ -5,44 +5,53 @@
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 class Account:
+    # 
     def __init__(self, name, acc_num, balance):
         self.set_name(name)
         self.set_acc_num(acc_num)
         self.set_balance(balance)
 
+    # returns name
     def get_name(self):
         return self._name
     
+    # 
     def set_name(self, name):
         if name != "":
             self._name = name
         else:
             raise ValueError("not a valid name")
 
+    # returns acc_num
     def get_acc_num(self):
         return self._acc_num
     
+    # 
     def set_acc_num(self, acc_num):
         if isinstance(acc_num, int) and acc_num >= 0:
                 self._acc_num = acc_num
         else:
             raise ValueError("Not a valid int")
 
+    # returns balence
     def get_balance(self):
         return self._balance
     
+    # 
     def set_balance(self, balance):
         if isinstance(balance, float, int) and balance >= 0:
                 self._acc_num = balance
         else:
             raise ValueError("Not a valid int")
-        
+    
+    # 
     def deposit(self, deposit_amount):
         if deposit_amount > 0:
             self._balance = self._balance + deposit_amount
         else:
             raise ValueError("Not a valid deposit")
-        
+    
+    # 
     def withdraw(self, withdraw_ammount):
         if withdraw_ammount > 0 and self._balance - withdraw_ammount >= 0:
             self._balance = self._balance - withdraw_ammount
--- bank.py
+++ bank.py
@@ -10,11 +10,38 @@ class Bank:
     def __init__(self):
         self._accounts = []
 
-    def add_account
-
+    # check if account exists and returns it, otherwise returns None
     def account_exist(self, acc_num):
         for account in self._accounts:
             if account.get_acc_num() == acc_num:
-                return True
+                return account
             else:
-                return False
+                return None
+
+    # usses the account exist function to add an account if it doesn't allready exists and dosn't if it allready exists
+    def add_account(self, name, acc_num):
+        if self.account_exist(acc_num) is not None:
+            raise ValueError("Account allready exists")
+        else:
+            new_account = Account(name, acc_num)
+            self._accounts.append(new_account)
+
+    # usses the account exist function to remove an account if it exists otherwise shows it can not be found
+    def remove_account(self, acc_num):
+        account = self.account_exist(acc_num)
+        if account:
+            self._accounts.remove(account)
+        else:
+            raise ValueError("Account not found")
+        
+    #
+    def deposit(self):
+        pass
+    
+    # 
+    def withdraw(self):
+        pass
+        
+    def transfer():
+        pass
+    
